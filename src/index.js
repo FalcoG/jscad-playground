8b3723f34009d3e74cdfaf55229890469526c263
Falco Gerritsjans <fcb319@alias.falco.pro>
2022-03-07 20:34:24 +0100

    feat: add typescript

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const { sphere } = require('@jscad/modeling').primitives
-
-const main = () => {
-    return sphere() // a single shape
-}
-
-module.exports = { main }
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { primitives } from '@jscad/modeling'
+
+const main = () => {
+    return primitives.sphere() // a single shape
+}
+
+export { main }
