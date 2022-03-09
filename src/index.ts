ba7ac0a0b44c3d36401ad57fb80da9d77fe93b93
Falco Gerritsjans <fcb319@alias.falco.pro>
2022-03-09 21:15:55 +0100

    feat: add eslint with typescript standard

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
-import { primitives, hulls, transforms } from '@jscad/modeling'
+import { primitives, hulls, transforms, extrusions } from '@jscad/modeling'
+import { Geom3 } from '@jscad/modeling/src/geometries/types'
 
-const main = () => {
-    const sphere = primitives.sphere({ radius: 1, center: [0, 0, 0] })
-    const sphere2 = primitives.sphere({ radius: 1, center: [2, 3, 0] })
+const main = (): Geom3 | Geom3[] => {
+  const deviceDimensions: [number, number, number] = [34.93, 24.07, 1.55]
+  const deviceScreenAngle = 120
+  const sphere = primitives.sphere({ radius: 1, center: [0, 0, 0] })
+  const sphere2 = primitives.sphere({ radius: 1, center: [2, 3, 0] })
 
-    let object = hulls.hull(sphere, sphere2)
-    object = transforms.translateY(5, object)
+  let object = hulls.hull(sphere, sphere2)
+  object = transforms.translateY(5, object)
 
-    const macbook = primitives.roundedCuboid({
-        size: [34.93, 1.55, 24.07],
-        roundRadius: 0.1}
-    )
+  // const device = primitives.roundedCuboid({
+  //   size: deviceDimensions,
+  //   roundRadius: 0.2
+  // })
 
-    return [object, macbook]
+  const radAngle = (180 - deviceScreenAngle) * Math.PI / 180
+  const stand = primitives.polygon({
+    points: [
+      [0, 0],
+      [deviceDimensions[1], 0],
+      [deviceDimensions[1] * Math.sin(radAngle), deviceDimensions[1] * Math.cos(radAngle)]
+    ]
+  })
+
+  const stand3D = extrusions.extrudeLinear({ height: 3 }, stand)
+
+  return [object, stand3D]
 }
 
 export { main }
