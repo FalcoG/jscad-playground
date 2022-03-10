597c8302e5bd7768a4e6c43798f00dc853f75f7b
Falco Gerritsjans <fcb319@alias.falco.pro>
2022-03-10 22:14:35 +0100

    feat: create device stand dynamically

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
-import { primitives, hulls, transforms, extrusions } from '@jscad/modeling'
+import { primitives, extrusions, utils } from '@jscad/modeling'
 import { Geom3 } from '@jscad/modeling/src/geometries/types'
 
 const main = (): Geom3 | Geom3[] => {
   const deviceDimensions: [number, number, number] = [34.93, 24.07, 1.55]
   const deviceScreenAngle = 120
-  const sphere = primitives.sphere({ radius: 1, center: [0, 0, 0] })
-  const sphere2 = primitives.sphere({ radius: 1, center: [2, 3, 0] })
+  const tilt = 35
 
-  let object = hulls.hull(sphere, sphere2)
-  object = transforms.translateY(5, object)
+  const device = primitives.roundedCuboid({
+    size: deviceDimensions,
+    roundRadius: 0.2,
+    center: [deviceDimensions[0] / 2, deviceDimensions[1] / 2, 0]
+  })
 
-  // const device = primitives.roundedCuboid({
-  //   size: deviceDimensions,
-  //   roundRadius: 0.2
-  // })
-
-  const radAngle = (180 - deviceScreenAngle) * Math.PI / 180
-  const stand = primitives.polygon({
-    points: [
-      [0, 0],
-      [deviceDimensions[1], 0],
-      [deviceDimensions[1] * Math.sin(radAngle), deviceDimensions[1] * Math.cos(radAngle)]
+  const top = 180 - deviceScreenAngle
+  const standAngle = top - tilt
+  const stand = primitives.triangle({
+    type: 'ASA',
+    values: [
+      utils.degToRad(standAngle),
+      deviceDimensions[1],
+      utils.degToRad(90 - standAngle)
     ]
   })
 
   const stand3D = extrusions.extrudeLinear({ height: 3 }, stand)
 
-  return [object, stand3D]
+  return [device, stand3D]
 }
 
 export { main }
